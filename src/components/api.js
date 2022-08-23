0a390750cd5aebb06508367befc4deb192294b4d
krishnainani <krishnainani4@gmail.com>
2022-08-23 12:05:46 +0100

    fetc topics sort the articles by topic

--- src/components/api.js
+++ src/components/api.js
@@ -6,3 +6,19 @@ export function fetchArticles () {
         return res.json();
       })
 }
+
+export function fetchTopics () {
+  return fetch(
+      "https://good-morning-news-app.herokuapp.com/api/topics"
+    ).then((res) => {
+      return res.json();
+    })
+}
+
+export function fetchArticlesByTopic (slug) {
+  return fetch(
+      `https://good-morning-news-app.herokuapp.com/api/articles?topic=${slug}`
+    ).then((res) => {
+      return res.json();
+    })
+}
